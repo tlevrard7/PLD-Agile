c6794db9063273bf63f7a94e2011bd167154c324
Spiky-73 <cocojeanne73@gmail.com>
2024-11-27 10:23:41 +0100

    added springboot project

---
+++ pickup-and-delivery-back/src/main/java/com/Bestanome/Main.java
@@ -0,0 +1,21 @@
+package com.Bestanome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class Main {
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+}
