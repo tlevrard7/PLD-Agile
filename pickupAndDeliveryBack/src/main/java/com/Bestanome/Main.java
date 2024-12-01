083bdc1f1ff1ccc9c71c8ea46dd921f1b5b89bf0
Spiky-73 <cocojeanne73@gmail.com>
2024-12-01 19:05:16 +0100

    allowed cors

--- pickupAndDeliveryBack/src/main/java/com/Bestanome/Main.java
+++ pickupAndDeliveryBack/src/main/java/com/Bestanome/Main.java
@@ -2,11 +2,22 @@ package com.Bestanome;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class Main {
+@Configuration
+@EnableWebMvc
+public class Main implements WebMvcConfigurer {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
 
+    @Override
+    public void addCorsMappings(@NonNull CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
 }
