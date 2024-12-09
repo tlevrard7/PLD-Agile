5d4f9a0a9a7cdd5de9fe4073a379f1bf00f6d2a8
Spiky-73 <cocojeanne73@gmail.com>
2024-12-09 09:38:30 +0100

    fix: IHM

--- pickup-and-delivery-front/src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-    return (
-      <div className="bg-blue-500 p-4 flex justify-center">
-        <button className="bg-green-500 text-white py-2 px-4 rounded">
-          Exporter les tournées
-        </button>
-      </div>
-    );
-  }
--- pickup-and-delivery-front/src/components/InteractiveMap.tsx
+++ pickup-and-delivery-front/src/components/InteractiveMap.tsx
@@ -1,29 +1,13 @@
-import { MapContainer, TileLayer, Marker, Polyline, Popup } from "react-leaflet";
+import { MapContainer, TileLayer, Polyline } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import { Plan } from "@/types/Plan";
-import L from "leaflet";
 import { useMemo } from "react";
 
 interface InteractiveMapProps {
   plan: Plan;
 }
 
-const customIcon = new L.Icon({
-  iconUrl: "/marker-icon.png",
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-});
-
 export default function InteractiveMap({ plan }: InteractiveMapProps) {
-  const markers = useMemo(
-    () =>
-      plan.points.map((point) => ({
-        id: point.id,
-        position: [point.latitude, point.longitude] as [number, number],
-      })),
-    [plan]
-  );
-
   const polylines = useMemo(
     () =>
       plan.segments
@@ -48,18 +32,6 @@ export default function InteractiveMap({ plan }: InteractiveMapProps) {
       style={{ width: "100%", height: "100%" }}
     >
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-
-      {markers.map((marker) => (
-        <Marker key={marker.id} position={marker.position} icon={customIcon}>
-          <Popup>
-            <div>
-              <p><strong>ID :</strong> {marker.id}</p>
-              <p><strong>Latitude :</strong> {marker.position[0]}</p>
-              <p><strong>Longitude :</strong> {marker.position[1]}</p>
-            </div>
-          </Popup>
-        </Marker>
-      ))}
 
       {polylines.map((polyline, index) => (
         <Polyline key={index} positions={polyline} color="blue" />
